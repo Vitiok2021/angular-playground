b3b48cc46bf99a1e7314825c07af7fd3d8e145a0
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-21 08:27:26 +0200

    added 3 buttons

--- counters/src/app/counter-child/counter-child.component.ts
+++ counters/src/app/counter-child/counter-child.component.ts
@@ -1,4 +1,12 @@
-import { Component, effect, Input, input, OnInit } from '@angular/core';
+import {
+  Component,
+  effect,
+  EventEmitter,
+  Input,
+  input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-counter-child',
@@ -8,16 +16,33 @@ import { Component, effect, Input, input, OnInit } from '@angular/core';
 })
 export class CounterChildComponent implements OnInit {
   counter: number = 0;
+  @Output() giveCount = new EventEmitter<number>();
 
-  @Input() initial: number = 0;
+  @Input() initial!: number;
+
   ngOnInit() {
     this.counter = this.initial;
+    this.giveCount.emit(this.counter);
   }
 
   increment() {
     this.counter = this.counter + 1;
+    this.giveCount.emit(this.counter);
   }
   decrement() {
     this.counter = this.counter - 1;
+    this.giveCount.emit(this.counter);
+  }
+  resetToStart() {
+    this.counter = this.initial;
+    this.giveCount.emit(this.counter);
+  }
+  resetToZero() {
+    this.counter = 0;
+    this.giveCount.emit(this.counter);
+  }
+  resetToCustom() {
+    this.counter = 50;
+    this.giveCount.emit(this.counter);
   }
 }
