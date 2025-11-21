9e5447510b3109db2616f04d77d90a418c9c8322
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-21 09:16:58 +0200

    finished own counter

--- counters/src/app/counter-child/counter-child.component.ts
+++ counters/src/app/counter-child/counter-child.component.ts
@@ -14,13 +14,33 @@ import {
   templateUrl: './counter-child.component.html',
   styleUrl: './counter-child.component.scss',
 })
-export class CounterChildComponent {
+export class CounterChildComponent implements OnInit {
   counterValue: number = 0;
 
+  @Input() fromInput!: number;
+  @Output() toOutput = new EventEmitter<number>();
+
+  ngOnInit(): void {
+    this.counterValue = this.fromInput;
+  }
   plusOne() {
     this.counterValue++;
+    this.toOutput.emit(this.counterValue);
   }
   minusOne() {
     this.counterValue--;
+    this.toOutput.emit(this.counterValue);
+  }
+  resToStart() {
+    this.counterValue = this.fromInput;
+    this.toOutput.emit(this.counterValue);
+  }
+  resToZero() {
+    this.counterValue = 0;
+    this.toOutput.emit(this.counterValue);
+  }
+  resToCust() {
+    this.counterValue = 50;
+    this.toOutput.emit(this.counterValue);
   }
 }
