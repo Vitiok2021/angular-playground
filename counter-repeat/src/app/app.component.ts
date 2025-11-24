fec378fee42134932327d48499d449091955f3d4
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-24 22:45:25 +0200

    installed material

---
+++ counter-repeat/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CounterParrentComponent } from './counter-parrent/counter-parrent.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CounterParrentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'counter-repeat';
+}
---
+++ counter-repeat/src/app/counter-child/counter-child.component.ts
@@ -0,0 +1,54 @@
+import {
+  Component,
+  input,
+  OnChanges,
+  OnInit,
+  output,
+  signal,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-counter-child',
+  imports: [],
+  templateUrl: './counter-child.component.html',
+  styleUrl: './counter-child.component.scss',
+})
+export class CounterChildComponent implements OnInit, OnChanges {
+  fromInput = input<number>(0); // дані вниз
+  counterId = input<number>(0); // дані вниз
+
+  counterValue = signal(0); // локальний стан
+
+  toOutput = output<{ id: number; value: number }>(); // подія наверх
+  remId = output<number>(); // подія remove
+
+  ngOnInit() {
+    this.counterValue.set(this.fromInput());
+  }
+  ngOnChanges() {
+    this.counterValue.set(this.fromInput());
+  }
+  plusOne() {
+    this.counterValue.update((x) => x + 1);
+    this.toOutput.emit({ id: this.counterId(), value: this.counterValue() });
+  }
+  minusOne() {
+    this.counterValue.update((x) => x - 1);
+    this.toOutput.emit({ id: this.counterId(), value: this.counterValue() });
+  }
+  resToStart() {
+    this.counterValue.set(this.fromInput());
+    this.toOutput.emit({ id: this.counterId(), value: this.counterValue() });
+  }
+  resToZero() {
+    this.counterValue.set(0);
+    this.toOutput.emit({ id: this.counterId(), value: this.counterValue() });
+  }
+  resToCust() {
+    this.counterValue.set(50);
+    this.toOutput.emit({ id: this.counterId(), value: this.counterValue() });
+  }
+  removeCounter() {
+    this.remId.emit(this.counterId());
+  }
+}
