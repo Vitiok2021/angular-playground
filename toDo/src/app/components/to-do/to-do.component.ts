509976b17b6e0426c5bc02578444c9228d6005d1
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-27 22:42:05 +0200

    feat: add html

--- toDo/src/app/components/to-do/to-do.component.ts
+++ toDo/src/app/components/to-do/to-do.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ToDosService } from '../../services/to-dos.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss',
 })
@@ -13,4 +15,14 @@ export class ToDoComponent {
   state = this.toDoService.state;
   completedCount = this.toDoService.completedCount;
   notCompletedCount = this.toDoService.notCompletedCount;
+  newTitle = '';
+  addToDo(title: string) {
+    this.toDoService.addTodo(title);
+  }
+  toggle(id: number) {
+    this.toDoService.toggleCompleted(id);
+  }
+  remove(id: number) {
+    this.toDoService.removeTodo(id);
+  }
 }
