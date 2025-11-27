321ea048dad9b59c2b8b9511b2ed5cff0e01423b
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-27 20:53:11 +0200

    feat: add inject and todo selectors

--- toDo/src/app/components/to-do/to-do.component.ts
+++ toDo/src/app/components/to-do/to-do.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToDosService } from '../../services/to-dos.service';
 
 @Component({
   selector: 'app-to-do',
   imports: [],
   templateUrl: './to-do.component.html',
-  styleUrl: './to-do.component.scss'
+  styleUrl: './to-do.component.scss',
 })
 export class ToDoComponent {
-
+  constructor() {}
+  private toDoService = inject(ToDosService);
+  state = this.toDoService.state;
+  completedCount = this.toDoService.completedCount;
+  notCompletedCount = this.toDoService.notCompletedCount;
 }
