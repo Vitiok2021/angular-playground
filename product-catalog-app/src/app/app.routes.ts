52ad592d72be8d7416a37fca04fd684f1a9b0c69
Vitiok2009 <viktorpts1vol@gmail.com>
2025-12-09 09:37:39 +0200

    feat: add new files to movie

--- product-catalog-app/src/app/app.routes.ts
+++ product-catalog-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsPageComponent } from './components/products-page/products-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductsPageComponent },
+  { path: 'cart', component: CartPageComponent },
+];
