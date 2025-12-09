dbd7fcd8656301e3abb85f84311d7652005b74eb
Vitiok2009 <viktorpts1vol@gmail.com>
2025-12-09 09:28:15 +0200

    feat: add movies array

--- movie-search-app/src/app/components/movies-page/movies-page.component.ts
+++ movie-search-app/src/app/components/movies-page/movies-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movies-page',
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './movies-page.component.html',
-  styleUrl: './movies-page.component.scss'
+  styleUrl: './movies-page.component.scss',
 })
 export class MoviesPageComponent {
-
+  movies = [
+    { id: 1, title: 'The matrix' },
+    { id: 2, title: 'Inception' },
+  ];
 }
