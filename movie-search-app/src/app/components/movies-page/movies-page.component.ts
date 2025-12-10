95044a011243168e1584e4811e0b3ccf91a7d522
Vitiok2009 <viktorpts1vol@gmail.com>
2025-12-10 18:48:05 +0200

    feat: add filtration movies

--- movie-search-app/src/app/components/movies-page/movies-page.component.ts
+++ movie-search-app/src/app/components/movies-page/movies-page.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
+import { FormsModule } from '@angular/forms';
+import { Movie } from '../../interfaces/movie';
 
 @Component({
   selector: 'app-movies-page',
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, FormsModule],
   templateUrl: './movies-page.component.html',
   styleUrl: './movies-page.component.scss',
 })
 export class MoviesPageComponent implements OnInit {
   constructor(private movieService: MoviesService) {}
-  movies: any | null = null;
+  movies: Movie[] = [];
   ngOnInit(): void {
     this.movies = this.movieService.movies;
+  }
+
+  searchTerm: string = '';
+  get filteredMovies() {
+    return this.movies.filter((movie) =>
+      movie.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
