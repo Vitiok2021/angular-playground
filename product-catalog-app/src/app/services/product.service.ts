00dc5fea1a3793e0fb595f2bbbcc87465a56617a
Vitiok2009 <viktorpts1vol@gmail.com>
2025-12-11 21:09:14 +0200

    feat: add UI to productCard

--- product-catalog-app/src/app/services/product.service.ts
+++ product-catalog-app/src/app/services/product.service.ts
@@ -13,21 +13,21 @@ export class ProductService {
       category: 'coffee',
       name: 'Lavaza',
       price: 205,
-      image: '',
+      image: 'https://picsum.photos/seed/coffee/200/200',
     },
     {
       id: 2,
       category: 'Tea',
       name: 'Azerchay',
       price: 95,
-      image: '',
+      image: 'https://picsum.photos/seed/tea/200/200',
     },
     {
       id: 3,
       category: 'Water',
       name: 'Coca Cola',
       price: 45,
-      image: '',
+      image: 'https://picsum.photos/seed/water/200/200',
     },
   ];
   getProductById(id: number) {
