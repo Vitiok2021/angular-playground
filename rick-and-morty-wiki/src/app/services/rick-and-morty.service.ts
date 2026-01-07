cad81e7538f84b787ea75021b8e8e7706811415e
Vitiok2009 <viktorpts1vol@gmail.com>
2026-01-07 20:27:42 +0200

    feat:add service, interface and HTTPClient

---
+++ rick-and-morty-wiki/src/app/services/rick-and-morty.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ApiResponse } from '../models/character';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RickAndMortyService {
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://rickandmortyapi.com/api/character';
+
+  getCharacters() {
+    return this.http.get<ApiResponse>(this.apiUrl);
+  }
+}
