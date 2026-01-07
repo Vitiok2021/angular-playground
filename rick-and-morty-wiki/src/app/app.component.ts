06f777e681dedb31b8ba740ce2c46e471cdc565c
Vitiok2009 <viktorpts1vol@gmail.com>
2026-01-07 20:41:03 +0200

    feat: add showCharters method

--- rick-and-morty-wiki/src/app/app.component.ts
+++ rick-and-morty-wiki/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RickAndMortyService } from './services/rick-and-morty.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rick-and-morty-wiki';
+
+  constructor() {
+    this.showCharters();
+  }
+
+  private rickAndMorty = inject(RickAndMortyService);
+
+  showCharters() {
+    this.rickAndMorty.getCharacters().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
