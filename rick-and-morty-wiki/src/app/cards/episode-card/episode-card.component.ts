e51348b534367e2e42ba591d3256d4db45491dfa
Vitiok2009 <viktorpts1vol@gmail.com>
2026-01-31 19:44:28 +0200

    feat: add favorites logic to episode-card

--- rick-and-morty-wiki/src/app/cards/episode-card/episode-card.component.ts
+++ rick-and-morty-wiki/src/app/cards/episode-card/episode-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-episode-card',
@@ -6,6 +6,33 @@ import { Component, Input } from '@angular/core';
   templateUrl: './episode-card.component.html',
   styleUrl: './episode-card.component.scss',
 })
-export class EpisodeCardComponent {
+export class EpisodeCardComponent implements OnInit {
   @Input() episode: any;
+  isFavorite: boolean = false;
+  ngOnInit(): void {
+    const favorites = this.getEpisodesFromStorage();
+    this.isFavorite = favorites.includes(this.episode.id);
+  }
+  toggleFavorite(event: Event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.isFavorite = !this.isFavorite;
+
+    const favorites = this.getEpisodesFromStorage();
+
+    if (this.isFavorite) {
+      favorites.push(this.episode.id);
+    } else {
+      const index = favorites.indexOf(this.episode.id);
+      if (index > -1) {
+        favorites.splice(index, 1);
+      }
+    }
+    localStorage.setItem('episodeFavorites', JSON.stringify(favorites));
+  }
+  getEpisodesFromStorage(): number[] {
+    const data = localStorage.getItem('episodeFavorites');
+    return data ? JSON.parse(data) : [];
+  }
 }
