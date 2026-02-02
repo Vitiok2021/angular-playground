fdc97f759b4bcac36ee44c83f7b04b9b35955146
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-02 20:24:45 +0200

    feat: add favorite-btn to episode-details

--- rick-and-morty-wiki/src/app/components/character-details/character-details.component.ts
+++ rick-and-morty-wiki/src/app/components/character-details/character-details.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { RickAndMortyService } from '../../services/rick-and-morty.service';
 import { Character } from '../../models/character';
+import { FavoriteBtnComponent } from '../favorite-btn/favorite-btn.component';
 
 @Component({
   selector: 'app-character-details',
-  imports: [RouterLink],
+  imports: [RouterLink, FavoriteBtnComponent],
   templateUrl: './character-details.component.html',
   styleUrl: './character-details.component.scss',
 })
@@ -13,41 +14,14 @@ export class CharacterDetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private rickAndMorty = inject(RickAndMortyService);
 
-  isFavorite: boolean = false;
-  currentId: number = 0;
-
   character: Character | null = null;
   ngOnInit(): void {
-    this.currentId = Number(this.route.snapshot.paramMap.get('id'));
-
-    if (this.currentId) {
-      const favorites = this.getFavorites();
-      this.isFavorite = favorites.includes(this.currentId);
-      this.rickAndMorty
-        .getCharacter(Number(this.currentId))
-        .subscribe((data) => {
-          this.character = data;
-          console.log(this.character);
-        });
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.rickAndMorty.getCharacter(Number(id)).subscribe((data) => {
+        this.character = data;
+        console.log(this.character);
+      });
     }
-  }
-  toggleFavorite(event: Event) {
-    this.isFavorite = !this.isFavorite;
-
-    const favorites = this.getFavorites();
-    if (this.isFavorite) {
-      favorites.push(this.currentId);
-    } else {
-      const index = favorites.indexOf(this.currentId);
-      if (index > -1) {
-        favorites.splice(index, 1);
-      }
-    }
-
-    localStorage.setItem('favoriteCharacters', JSON.stringify(favorites));
-  }
-  getFavorites() {
-    const data = localStorage.getItem('favoriteCharacters');
-    return data ? JSON.parse(data) : [];
   }
 }
--- rick-and-morty-wiki/src/app/components/episode-details/episode-details.component.ts
+++ rick-and-morty-wiki/src/app/components/episode-details/episode-details.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { RickAndMortyService } from '../../services/rick-and-morty.service';
 import { Episode } from '../../models/character';
+import { FavoriteBtnComponent } from '../favorite-btn/favorite-btn.component';
 
 @Component({
   selector: 'app-episode-details',
-  imports: [RouterLink],
+  imports: [RouterLink, FavoriteBtnComponent],
   templateUrl: './episode-details.component.html',
   styleUrl: './episode-details.component.scss',
 })
@@ -15,36 +16,13 @@ export class EpisodeDetailsComponent implements OnInit {
 
   episode: Episode | null = null;
 
-  isFavorite: boolean = false;
-  currentId: number = 0;
-
   ngOnInit(): void {
-    this.currentId = Number(this.route.snapshot.paramMap.get('id'));
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    if (this.currentId) {
-      const favorites = this.getFavoritesFromLocalStorage();
-      this.isFavorite = favorites.includes(this.currentId);
-      this.rickAndMorty.getEpisode(Number(this.currentId)).subscribe((data) => {
+    if (id) {
+      this.rickAndMorty.getEpisode(Number(id)).subscribe((data) => {
         this.episode = data;
       });
     }
-  }
-  toggleFavorite(event: Event) {
-    this.isFavorite = !this.isFavorite;
-    const favorites = this.getFavoritesFromLocalStorage();
-
-    if (this.isFavorite) {
-      favorites.push(this.currentId);
-    } else {
-      const index = favorites.indexOf(this.currentId);
-      if (index > -1) {
-        favorites.splice(index, 1);
-      }
-    }
-    localStorage.setItem('favoriteEpisodes', JSON.stringify(favorites));
-  }
-  getFavoritesFromLocalStorage() {
-    const data = localStorage.getItem('favoriteEpisodes');
-    return data ? JSON.parse(data) : [];
   }
 }
