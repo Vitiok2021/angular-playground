fe00d47a78a5b31a5bcee6b9ebf0b4d914476c41
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-06 20:48:22 +0200

    feat: add live search functionality

--- crypto-tracker/src/app/components/coins-list/coins-list.component.ts
+++ crypto-tracker/src/app/components/coins-list/coins-list.component.ts
@@ -2,16 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CoinCardComponent } from '../coin-card/coin-card.component';
 import { CryptoService } from '../../services/crypto.service';
 import { Coin } from '../../models/coin';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-coins-list',
-  imports: [CoinCardComponent],
+  imports: [CoinCardComponent, FormsModule],
   templateUrl: './coins-list.component.html',
   styleUrl: './coins-list.component.scss',
 })
 export class CoinsListComponent implements OnInit {
   private cryptoService = inject(CryptoService);
   coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  searchText = '';
 
   ngOnInit(): void {
     this.loadCoin();
@@ -20,7 +23,17 @@ export class CoinsListComponent implements OnInit {
   loadCoin() {
     this.cryptoService.getCoins().subscribe((data) => {
       this.coins = data;
+      this.filteredCoins = data;
       console.log(data);
+    });
+  }
+
+  handleSearch() {
+    this.filteredCoins = this.coins.filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+      );
     });
   }
 }
