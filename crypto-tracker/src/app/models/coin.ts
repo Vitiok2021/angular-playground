9346a917708e5cf200834ee91f902834859dfe53
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-07 19:06:05 +0200

    refactor: change view detail

--- crypto-tracker/src/app/models/coin.ts
+++ crypto-tracker/src/app/models/coin.ts
@@ -29,5 +29,17 @@ export interface CoinDetail {
     };
     market_cap_rank: number;
     price_change_percentage_24h: number;
+    market_cap: {
+      usd: number;
+    };
+    total_volume: {
+      usd: number;
+    };
+    high_24h: {
+      usd: number;
+    };
+    low_24h: {
+      usd: number;
+    };
   };
 }
