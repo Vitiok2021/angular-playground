7f8296bf1906d718728ca608957951239c0913d8
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-08 22:27:08 +0200

    feat: add getFavoriteCoins to crypto-service

--- crypto-tracker/src/app/app.routes.ts
+++ crypto-tracker/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoinsListComponent } from './components/coins-list/coins-list.component';
 import { CoinDetailComponent } from './components/coin-detail/coin-detail.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
 export const routes: Routes = [
   { path: '', component: CoinsListComponent },
   { path: 'list/', component: CoinsListComponent },
   { path: 'coin/:id', component: CoinDetailComponent },
+  { path: 'favorites', component: FavoritesComponent },
 ];
--- crypto-tracker/src/app/services/crypto.service.ts
+++ crypto-tracker/src/app/services/crypto.service.ts
@@ -7,14 +7,17 @@ import { Coin, CoinDetail } from '../models/coin';
 })
 export class CryptoService {
   private http = inject(HttpClient);
-  private apiUrl =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false';
-
+  private baseUrl = 'https://api.coingecko.com/api/v3/coins/markets';
   constructor() {}
 
   getCoins(currency: string, page: number = 1) {
-    const url = `${this.apiUrl}?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=${page}&sparkline=false`;
+    const url = `${this.baseUrl}?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=${page}&sparkline=false`;
     return this.http.get<Coin[]>(url);
+  }
+  getFavoriteCoins(ids: string[]) {
+    const idsString = ids.join(',');
+    const url = `${this.baseUrl}?vs_currency=usd&ids=${idsString}&order=market_cap_desc&sparkline=false`;
+    return this.http.get<CoinDetail>(url);
   }
   getCoin(coinId: string) {
     const url = `https://api.coingecko.com/api/v3/coins/${coinId}`;
