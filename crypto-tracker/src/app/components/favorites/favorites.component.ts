4e934447a063831755a62eb2d99bf05aa4ff48c5
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-10 22:11:03 +0200

    docs: minor update to favorites empty state

--- crypto-tracker/src/app/components/favorites/favorites.component.ts
+++ crypto-tracker/src/app/components/favorites/favorites.component.ts
@@ -3,10 +3,11 @@ import { StorageService } from '../../services/storage.service';
 import { CryptoService } from '../../services/crypto.service';
 import { Coin } from '../../models/coin';
 import { CoinCardComponent } from '../coin-card/coin-card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [CoinCardComponent],
+  imports: [CoinCardComponent, RouterLink],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss',
 })
