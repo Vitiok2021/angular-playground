d80c9c5a960147e85109e95b7c30097a63c47d61
Vitiok2009 <viktorpts1vol@gmail.com>
2026-02-22 20:50:32 +0200

    feat: added delTask() and toggleIsDone()

--- toDoList/src/app/components/to-do-view/to-do-view.component.ts
+++ toDoList/src/app/components/to-do-view/to-do-view.component.ts
@@ -11,12 +11,20 @@ export class ToDoViewComponent {
   task!: string;
   tasks: Task[] = [];
   addTask() {
-    const id = new Date().getMilliseconds();
+    const id = Date.now();
     const isDone = false;
 
     if (this.task) {
       this.tasks.push({ id: id, name: this.task, isDone: isDone });
+      this.task = '';
     }
+  }
+  delTask(id: number) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  toggleIsDone(id: number) {
+    const foundedTask = this.tasks.find((task) => task.id === id);
+    if (foundedTask) foundedTask.isDone = !foundedTask.isDone;
   }
 }
 export interface Task {
