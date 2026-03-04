0ce9298c3bde51717ce5cfd7e6cf973e601e83ab
Vitiok2009 <viktorpts1vol@gmail.com>
2026-03-04 19:57:17 +0200

    feat: added animation fade out/fadein

--- toDoList/src/app/components/to-do-view/to-do-view.component.ts
+++ toDoList/src/app/components/to-do-view/to-do-view.component.ts
@@ -3,11 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { ToDoItemComponent } from '../to-do-item/to-do-item.component';
 import { Task } from '../../interfaces/task';
 import { ToDoService } from '../../services/to-do.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-to-do-view',
   imports: [FormsModule, ToDoItemComponent],
   templateUrl: './to-do-view.component.html',
   styleUrl: './to-do-view.component.css',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('500ms ease-in', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ToDoViewComponent {
   task!: string;
