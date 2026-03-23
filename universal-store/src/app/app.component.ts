d8827cdccf334ca1e57418fbe1215c5700e23a6a
Vitiok2009 <viktorpts1vol@gmail.com>
2026-03-23 08:02:56 +0200

    feat: ended hero section

--- universal-store/src/app/app.component.ts
+++ universal-store/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
