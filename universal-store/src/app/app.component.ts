3aeb5759481ae6b79858abe7c856711cf919230d
Vitiok2009 <viktorpts1vol@gmail.com>
2026-03-25 14:40:23 +0200

    refactor: moved section catalog and logic to catalog.component

--- universal-store/src/app/app.component.ts
+++ universal-store/src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ProductService } from './services/product.service';
-import { ProductCard } from './interfaces/product-card';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { CatalogComponent } from './pages/catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +12,9 @@ import { AsyncPipe } from '@angular/common';
     HeaderComponent,
     FooterComponent,
     HeroComponent,
-    ProductCardComponent,
-    AsyncPipe,
+    CatalogComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  productService = inject(ProductService);
-  fishingProducts: Observable<ProductCard[]> =
-    this.productService.getProducts();
-}
+export class AppComponent {}
--- universal-store/src/app/pages/catalog/catalog.component.ts
+++ universal-store/src/app/pages/catalog/catalog.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { ProductCard } from '../../interfaces/product-card';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [ProductCardComponent, AsyncPipe],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
-
+  productService = inject(ProductService);
+  fishingProducts: Observable<ProductCard[]> =
+    this.productService.getProducts();
 }
