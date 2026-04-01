2246870c436de57e2bae6ca6f0507d9b8ee0e5d2
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-01 09:22:32 +0300

    feat: add saved to localStorage

--- universal-store/src/app/services/cart.service.ts
+++ universal-store/src/app/services/cart.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ProductDetails } from '../interfaces/product-details';
 import { CartItem } from '../interfaces/cart-item';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  private platformId = inject(PLATFORM_ID);
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedCart = localStorage.getItem('fishing_cart');
+
+      if (savedCart) {
+        this.cartItemsSubject.next(JSON.parse(savedCart));
+      }
+    }
+  }
   private cartItemsSubject = new BehaviorSubject<CartItem[]>([]);
 
   cartItems$ = this.cartItemsSubject.asObservable();
@@ -26,5 +37,9 @@ export class CartService {
     }
     this.cartItemsSubject.next(currentItems);
     // console.log('Кошик зараз:', currentItems);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('fishing_cart', JSON.stringify(currentItems));
+    }
   }
 }
