313d6eb8874dcac641ce522d640a3986d7872c28
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-05 10:54:47 +0300

    feat: implement cart items and showed items in sidebar

--- universal-store/src/app/components/cart-sidebar/cart-sidebar.component.ts
+++ universal-store/src/app/components/cart-sidebar/cart-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../interfaces/cart-item';
 
 @Component({
   selector: 'app-cart-sidebar',
@@ -11,8 +12,12 @@ export class CartSidebarComponent implements OnInit {
   cartService = inject(CartService);
   isOpen = false;
 
+  cartItems: CartItem[] = [];
+
   ngOnInit(): void {
     this.cartService.isCartOpen$.subscribe((status) => (this.isOpen = status));
+
+    this.cartService.cartItems$.subscribe((data) => (this.cartItems = data));
   }
 
   closeCart() {
