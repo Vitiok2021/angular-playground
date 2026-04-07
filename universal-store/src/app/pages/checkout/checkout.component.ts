8dd3b4f1d4be212c930c00ebecfdeec293e81185
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-07 19:30:44 +0300

    feat: add form component

---
+++ universal-store/src/app/pages/checkout/checkout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [ReactiveFormsModule],
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.scss',
+})
+export class CheckoutComponent {}
