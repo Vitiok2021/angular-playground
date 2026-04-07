2e0e501c4a781c04d02d18455f5515091d8c1662
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-07 18:26:18 +0300

    style: change animation to added product

--- universal-store/src/app/components/toast/toast.component.ts
+++ universal-store/src/app/components/toast/toast.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
-
+import { tap } from 'rxjs';
 @Component({
   selector: 'app-toast',
   imports: [],
@@ -10,14 +10,22 @@ import { NotificationService } from '../../services/notification.service';
 export class ToastComponent {
   notificationService = inject(NotificationService);
 
+  isHiding: boolean = false;
   currentMessage: string = '';
 
   constructor() {
-    this.notificationService.message$.subscribe((data) => {
-      this.currentMessage = data;
-      setTimeout(() => {
-        this.currentMessage = '';
-      }, 3000);
-    });
+    this.notificationService.message$
+      .pipe(tap((data) => console.log('Шпигун tap зловив повідомлення:', data)))
+      .subscribe((data) => {
+        this.currentMessage = data;
+        this.isHiding = false;
+        setTimeout(() => {
+          this.isHiding = true;
+
+          setTimeout(() => {
+            this.currentMessage = '';
+          }, 300);
+        }, 3000);
+      });
   }
 }
