74085ffce9155e8846677204a9bdeec4c20357f7
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-21 19:08:47 +0300

    feat: add favorite logic to product-card

--- universal-store/src/app/components/product-card/product-card.component.ts
+++ universal-store/src/app/components/product-card/product-card.component.ts
@@ -1,21 +1,47 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { ProductCard } from '../../interfaces/product-card';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { ProductDetails } from '../../interfaces/product-details';
+import { FavoriteService } from '../../services/favorite.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, AsyncPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
   cartService = inject(CartService);
+  favoriteService = inject(FavoriteService);
 
   @Input() prodCard!: ProductCard;
   @Input() isAdmin: boolean = false;
+
+  isFavorites$!: Observable<boolean>;
+  ngOnInit(): void {
+    this.isFavorites$ = this.favoriteService.favorites.pipe(
+      map((favoritesArray) => {
+        const searchedItem = favoritesArray.find(
+          (item: ProductCard) => item.id === this.prodCard.id,
+        );
+        if (searchedItem) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
+    );
+  }
 
   @Output() onDelete = new EventEmitter();
   onDeleteProduct() {
@@ -28,5 +54,8 @@ export class ProductCardComponent {
   }
   addCart(product: ProductCard) {
     this.cartService.addToCart(product);
+  }
+  addToFavorite(product: ProductCard) {
+    this.favoriteService.toggle(product);
   }
 }
