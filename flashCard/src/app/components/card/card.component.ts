3dd5e2969b187e9eff6e1aba3acbf0cf9093beb4
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-27 09:42:08 +0300

    feat: add searching reactive form

--- flashCard/src/app/components/card/card.component.ts
+++ flashCard/src/app/components/card/card.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  cardForm = new FormGroup({
+    word: new FormControl('', Validators.minLength(2)),
+    translate: new FormControl('', Validators.minLength(2)),
+  });
+  onTranslate() {}
 }
