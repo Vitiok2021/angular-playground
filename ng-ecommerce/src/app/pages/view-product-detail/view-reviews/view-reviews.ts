8ca4b0db7fb27bf66972bf1d9e4bc84c74739ab4
vitiok2021 <viktorpts1vol@gmail.com>
2026-08-14 22:23:46 +0300

    feat: finished user reviews with sorting on a date

--- ng-ecommerce/src/app/pages/view-product-detail/view-reviews/view-reviews.ts
+++ ng-ecommerce/src/app/pages/view-product-detail/view-reviews/view-reviews.ts
@@ -1,21 +1,33 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Product } from '../../../models/product';
 import { ViewPanel } from '../../../directives/view-panel';
 import { RatingSummary } from '../rating-summary/rating-summary';
+import { ViewReviewItem } from '../view-review-item/view-review-item';
 
 @Component({
   selector: 'app-view-reviews',
-  imports: [ViewPanel, RatingSummary],
+  imports: [ViewPanel, RatingSummary, ViewReviewItem],
   template: `
     <div appViewPanel>
       <div class="flex justify-between items-center mb-4">
         <h2 class="text-xl font-semibold">Customer Reviews</h2>
       </div>
       <app-rating-summary [product]="product()"></app-rating-summary>
+      <div class="flex flex-col gap-6">
+        @for (review of sortedReviews(); track review.id) {
+          <app-view-review-item [review]="review"></app-view-review-item>
+        }
+      </div>
     </div>
   `,
   styles: ``,
 })
 export class ViewReviews {
   product = input.required<Product>();
+
+  sortedReviews = computed(() => {
+    return [...this.product().reviews].sort(
+      (a, b) => b.reviewDate.getTime() - a.reviewDate.getTime(),
+    );
+  });
 }
