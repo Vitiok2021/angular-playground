60ce9c741258d2cd4d801e0272da8fa5630ccd6d
vitiok2021 <viktorpts1vol@gmail.com>
2026-08-16 17:28:14 +0300

    feat: finished ssr settings

--- ng-ecommerce/src/app/app.routes.server.ts
+++ ng-ecommerce/src/app/app.routes.server.ts
@@ -1,8 +1,27 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { CategoryApi } from './services/category-api';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:categoryName',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const catService = inject(CategoryApi);
+      const names = catService.getCategories();
+      return names.map((name) => ({ category: name }));
+    },
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Client,
+  },
+  { path: 'cart', renderMode: RenderMode.Client },
+  { path: 'checkout', renderMode: RenderMode.Client },
+  { path: 'order-success', renderMode: RenderMode.Client },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
