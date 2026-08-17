e99c0a801428d7c5efa95a7cfccdf993e3df69ff
vitiok2021 <viktorpts1vol@gmail.com>
2026-08-17 18:42:52 +0300

    feat: add local dictionary translation

--- flashCard/src/app/components/card/card.component.ts
+++ flashCard/src/app/components/card/card.component.ts
@@ -13,12 +13,24 @@ import {
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  dictionary = {
+    attach: 'прикріпляти',
+    attachment: 'вкладення',
+    access: 'доступ',
+    crash: 'збій',
+  };
   cardForm = new FormGroup({
     word: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    translate: new FormControl('', [
-      Validators.required,
-      Validators.minLength(2),
-    ]),
+    translate: new FormControl(''),
   });
-  onTranslate() {}
+  onTranslate() {
+    if (!this.cardForm.valid) return;
+    const currentWord = this.cardForm.value.word?.trim().toLowerCase() || '';
+    const translatedWord = (this.dictionary as any)[currentWord];
+    if (translatedWord) {
+      this.cardForm.controls.translate.setValue(translatedWord);
+    } else {
+      this.cardForm.controls.translate.setValue('Not Found');
+    }
+  }
 }
