fa3969883ba0638a2e40ff4857925ea72a2f9c05
vitiok2021 <viktorpts1vol@gmail.com>
2026-08-28 12:06:16 +0300

    feat(api): setup environments and mock data toggle

--- electronics-store/src/app/shared/api/api.service.ts
+++ electronics-store/src/app/shared/api/api.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../../entities/product/models/product.interface';
+import { environment } from '../../../environments/environment';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,18 @@ export class ApiService {
   http = inject(HttpClient);
 
   getProducts() {
-    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+    if (environment.useMockData) {
+      return of<Product[]>([
+        {
+          id: 1,
+          title: 'Mock Title',
+          price: 1,
+          description: 'mock description',
+          category: 'mock category',
+          image: 'mock image',
+        },
+      ]);
+    }
+    return this.http.get<Product[]>(environment.apiUrl);
   }
 }
