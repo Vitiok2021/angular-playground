3de12d04da39fc21b605bd87e9557c3820a877de
vitiok2021 <viktorpts1vol@gmail.com>
2026-09-06 13:25:51 +0300

    feat: add global auth interceptor

--- electronics-store/src/app/app.config.ts
+++ electronics-store/src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './shared/api/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
 };
---
+++ electronics-store/src/app/shared/api/interceptors/auth-interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.url.includes('auth')) return next(req);
+
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    const cloneReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloneReq);
+  }
+  return next(req);
+};
